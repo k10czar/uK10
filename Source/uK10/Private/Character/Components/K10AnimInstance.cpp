7826b2911996f99db378e065975654508b64fe31
K10 <k10.czar@gmail.com>
2023-05-07 16:17:23 -0300

    Anim first steps

---
+++ Source/uK10/Private/Character/Components/K10AnimInstance.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Components/K10AnimInstance.h"
+#include "Character/K10CharacterBase.h"
+#include "Log/K10Macros.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UK10AnimInstance::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+    _character = Cast<AK10CharacterBase>(TryGetPawnOwner());
+}
+
+void UK10AnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+    Super::NativeUpdateAnimation(DeltaTime);
+
+    if (!_character) return;
+
+    FVector Velocity = _character->GetVelocity();
+    Velocity.Z = 0.f;
+    Speed = Velocity.Size();
+
+    bIsInAir = _character->GetCharacterMovement()->IsFalling();
+    bIsAccelerating = _character->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+    bIsCrouched = _character->bIsCrouched;
+
+    // Offset Yaw for Strafing
+    FRotator AimRotation = _character->GetBaseAimRotation();
+    FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(_character->GetVelocity());
+    FRotator DeltaRot = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation);
+    DeltaRotation = FMath::RInterpTo(DeltaRotation, DeltaRot, DeltaTime, 6.f);
+    YawOffset = DeltaRotation.Yaw;
+
+    CharacterRotationLastFrame = CharacterRotation;
+    CharacterRotation = _character->GetActorRotation();
+    const FRotator Delta = UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame);
+    const float Target = Delta.Yaw / DeltaTime;
+    const float Interp = FMath::FInterpTo(Lean, Target, DeltaTime, 6.f);
+    Lean = FMath::Clamp(Interp, -90.f, 90.f);
+}
---
+++ Source/uK10/Public/Character/Components/K10AnimInstance.h
@@ -0,0 +1,117 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "K10AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UK10_API UK10AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+    // friend class AShooterCharacter;
+    virtual void NativeInitializeAnimation() override;
+    virtual void NativeUpdateAnimation(float DeltaTime) override;
+	
+protected:
+    UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+    class AK10CharacterBase* _character;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* FireWeaponMontage;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* ReloadMontage;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* HitReactMontage;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* ElimMontage;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* ThrowGrenadeMontage;
+
+    UPROPERTY(EditAnywhere)
+    UAnimMontage* SwapMontage;
+
+
+    /* Basic animation variables */
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    float Speed;
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    float YawOffset;
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    bool bIsInAir;
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    bool bIsAccelerating;
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    bool bIsCrouched;
+
+    UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    float Lean;
+
+    /* Related to weapons */
+
+    // class AWeapon* EquippedWeapon;
+
+    UPROPERTY(BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
+    bool bWeaponEquipped;
+
+    UPROPERTY(BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
+    bool bAiming;
+
+    UPROPERTY(BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
+    bool bUseAimOffsets;
+
+    UPROPERTY(BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
+    float AO_Yaw;
+
+    UPROPERTY(BlueprintReadOnly, Category = Weapon, meta = (AllowPrivateAccess = "true"))
+    float AO_Pitch;
+
+    /* Other gameplay variables */
+
+    UPROPERTY(BlueprintReadOnly, Category = Gameplay, meta = (AllowPrivateAccess = "true"))
+    bool bElimmed;
+
+    UPROPERTY(BlueprintReadOnly, Category = Gameplay, meta = (AllowPrivateAccess = "true"))
+    bool bHoldingTheFlag;
+
+    /* About turning, transform */
+
+    FRotator CharacterRotationLastFrame;
+    FRotator CharacterRotation;
+    FRotator DeltaRotation;
+
+    // UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    // ETurningInPlace TurningInPlace;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    FTransform LeftHandTransform;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    FRotator RightHandRotation;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    bool bLocallyControlled;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    bool bRotateRootBone;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    bool bUseFABRIK;
+
+    UPROPERTY(BlueprintReadOnly, Category = Turning, meta = (AllowPrivateAccess = "true"))
+    bool bTransformRightHand;
+};
